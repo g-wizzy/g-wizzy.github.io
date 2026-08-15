82469030a4a6f665278c03808a109ebe3e468532
g-wizzy <pierre.burki@protonmail.com>
2026-08-15 12:30:17 +0200

    Change page to display dnd-stuff instead

--- filtering/__init__.py
+++
@@ -1,1 +0,0 @@
-from .main import is_good_word
--- main.js
+++
@@ -1,9 +0,0 @@
-function update() {
-    const word = words[Math.floor(Math.random() * words.length)];
-    document.getElementById("text").innerText = word;
-}
-
-window.onload = () => {
-    update();
-    document.getElementById("page").onclick = update;
-}
